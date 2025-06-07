c2356ec917d0514ae335d3fe15ee113e0d62b4f3
Rafael Reyes <134435660+rafa-reyes04@users.noreply.github.com>
2025-06-07 10:16:27 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const repo = 'CodeArena';
+
+const nextConfig = {
+  output: 'export',
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true, // Recomendado para compatibilidad con GitHub Pages
+};
 
 export default nextConfig;
